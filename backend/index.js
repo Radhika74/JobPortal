f2c366b8b4b04f48952afa74e6d300f6f31c1eed
Radhika Thakur <radhikathakur1103@gmail.com>
2025-04-28 16:38:18 +0530

    Backend functionlity working properly

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js"; 
+import express from "express";
+import applicationRoute from "./routes/application.routes.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import userRoute from "./routes/user.route.js";
+import connectDB from "./utilis/db.js";
+
 dotenv.config({});
 
 const app = express();
@@ -26,7 +31,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT=process.env.PORT || 3000;
+
+//API'S
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
 app.listen(PORT,()=>{
-    connectB();
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
