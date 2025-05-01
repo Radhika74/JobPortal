7e58e3d7628124c07c8027db2231e48e8d0fd7ff
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-01 22:37:06 +0530

    NAvbar profile issues to be pending for resolve

--- fronted/src/components/ui/shared/Navbar.jsx
+++ fronted/src/components/ui/shared/Navbar.jsx
@@ -1,32 +1,33 @@
-import { setUser } from '@/redux/authSlice'
-import { USER_API_END_POINT } from '@/utils/constant'
-import axios from 'axios'
+//import { setUser } from '@/redux/authSlice'
+//import { USER_API_END_POINT } from '@/utils/constant'
+//import axios from 'axios'
 import { LogOut, User2 } from 'lucide-react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import { toast } from 'sonner'
+//import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+// import { toast } from 'sonner'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 
 const Navbar = () => {
-    const { user } = useSelector(store => store.auth);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+    //const { user } = useSelector(store => store.auth);
+    const user =true;
+    // const dispatch = useDispatch();
+    // const navigate = useNavigate();
 
-    const logoutHandler = async () => {
-        try {
-            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
-            if (res.data.success) {
-                dispatch(setUser(null));
-                navigate("/");
-                toast.success(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        }
-    }
+    // const logoutHandler = async () => {
+    //     try {
+    //         const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+    //         if (res.data.success) {
+    //             dispatch(setUser(null));
+    //             navigate("/");
+    //             toast.success(res.data.message);
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //         toast.error(error.response.data.message);
+    //     }
+    // }
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -69,26 +70,30 @@ const Navbar = () => {
                                     <div className=''>
                                         <div className='flex gap-2 space-y-2'>
                                             <Avatar className="cursor-pointer">
-                                                <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn" />
+                                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
                                             </Avatar>
                                             <div>
-                                                <h4 className='font-medium'>{user?.fullname}</h4>
-                                                <p className='text-sm text-muted-foreground'>{user?.profile?.bio}</p>
+                                                <h4 className='font-medium'>Radhika</h4>
+                                                <p className='text-sm text-muted-foreground'>loreum impsum dolor sit amet.</p>
                                             </div>
                                         </div>
                                         <div className='flex flex-col my-2 text-gray-600'>
-                                            {
+                                            {/* {
                                                 user && user.role === 'student' && (
                                                     <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                                         <User2 />
                                                         <Button variant="link"> <Link to="/profile">View Profile</Link></Button>
                                                     </div>
                                                 )
-                                            }
+                                            } */}
 
                                             <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                                <User2 />
+                                                <Button variant="link">View Profile</Button>
+                                            </div>
+                                            <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                                 <LogOut />
-                                                <Button onClick={logoutHandler} variant="link">Logout</Button>
+                                                <Button  variant="link">Logout</Button>
                                             </div>
                                         </div>
                                     </div>
