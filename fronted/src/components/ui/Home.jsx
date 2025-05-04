bb0006aa8b657ee0f6cea98ecafcde28132f4ffa
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-04 23:20:44 +0530

    Routes of admin and jobs

--- fronted/src/components/ui/Home.jsx
+++ fronted/src/components/ui/Home.jsx
@@ -1,19 +1,31 @@
-import CategoryCarousel from "./CategoryCarousal";
-import Footer from "./Footer";
-import HeroSection from "./HeroSection";
-import LatestJobs from "./Latestjobs";
-import Navbar from "./shared/Navbar";
+import useGetAllJobs from '@/hooks/useGetAllJobs'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import CategoryCarousel from './CategoryCarousal'
+import Footer from './Footer'
+import HeroSection from './HeroSection'
+import LatestJobs from './LatestJobs'
+import Navbar from './shared/Navbar'
 
 const Home = () => {
+    useGetAllJobs();
+    const { user } = useSelector(store => store.auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user?.role === 'recruiter') {
+            navigate("/admin/companies");
+        }
+    }, []);
     return (
-    <div>
-            <Navbar/>
-            <HeroSection/>
-            <CategoryCarousel/>
-            <LatestJobs/>
-            <Footer/>
-    </div>
+        <div>
+            <Navbar />
+            <HeroSection />
+            <CategoryCarousel />
+            <LatestJobs />
+            <Footer />
+        </div>
     )
-};
+}
 
-export default Home;
+export default Home
