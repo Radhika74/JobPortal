2ed6cda475c93ca46fce9a7107cd5b76015832b3
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-05 23:26:17 +0530

    Company model

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -7,13 +7,13 @@ const companySchema = new mongoose.Schema({
         unique:true
     },
     description:{
-        type:String,
+        type:String, 
     },
     website:{
-        type:String
+        type:String 
     },
     location:{
-        type:String
+        type:String 
     },
     logo:{
         type:String // URL to company logo
