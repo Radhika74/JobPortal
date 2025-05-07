147368762454ccdc3352ccbddb530e0b91a90e92
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-07 19:38:37 +0530

    Modified app and main after setting up clouidnary

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,51 +1,83 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './components/ui/auth/Login';
-import Signup from './components/ui/auth/Signup';
-import Browse from './components/ui/Browse';
-import Home from './components/ui/Home';
-import JobDescription from './components/ui/JobDescription';
-import Jobs from './components/ui/Jobs';
-import Profile from './components/ui/Profile';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Browse from './components/ui/Browse'
+import Home from './components/ui/Home'
+import JobDescription from './components/ui/JobDescription'
+import Jobs from './components/ui/Jobs'
+import Profile from './components/ui/Profile'
+import AdminJobs from './components/ui/admin/AdminJobs'
+import Applicants from './components/ui/admin/Applicants'
+import Companies from './components/ui/admin/Companies'
+import CompanyCreate from './components/ui/admin/CompanyCreate'
+import CompanySetup from './components/ui/admin/CompanySetup'
+import PostJob from './components/ui/admin/PostJob'
+import ProtectedRoute from './components/ui/admin/ProtectedRoute'
+import Login from './components/ui/auth/Login'
+import Signup from './components/ui/auth/Signup'
+
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,  // Set a default page (e.g., Home or a Landing component)
-  },
-  {
-    path: '/Home',
-    element: <Home />,
+    element: <Home />
   },
   {
     path: '/login',
-    element: <Login />,
+    element: <Login />
   },
   {
     path: '/signup',
-    element: <Signup />,
+    element: <Signup />
   },
   {
-    path:'/Jobs',
-    element: <Jobs/>
+    path: "/jobs",
+    element: <Jobs />
   },
   {
-    path:'/description/:id',
-    element: <JobDescription/>
+    path: "/description/:id",
+    element: <JobDescription />
   },
   {
-    path:'/Browse',
-    element: <Browse/>
+    path: "/browse",
+    element: <Browse />
   },
   {
-    path:'/Profile',
-    element: <Profile/>
-  }
-]);
+    path: "/profile",
+    element: <Profile />
+  },
+  // admin ke liye yha se start hoga
+  {
+    path:"/admin/companies",
+    element: <ProtectedRoute><Companies/></ProtectedRoute>
+  },
+  {
+    path:"/admin/companies/create",
+    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies/:id",
+    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs",
+    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/:id/applicants",
+    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  },
 
+])
 function App() {
+
   return (
+    <div>
       <RouterProvider router={appRouter} />
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -1,17 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { Toaster } from 'sonner'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.jsx'
+import { Toaster } from './components/ui/ui/sonner.jsx'
 import './index.css'
 import store from './redux/store.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-    <Toaster/>
+const persistor = persistStore(store);
 
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 )
