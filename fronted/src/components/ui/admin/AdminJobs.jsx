e7c2f67c6be9a661a4c8a017ee7642054b728261
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-10 23:50:35 +0530

    creating jobs

--- fronted/src/components/ui/admin/AdminJobs.jsx
+++ fronted/src/components/ui/admin/AdminJobs.jsx
@@ -19,18 +19,18 @@ const AdminJobs = () => {
   }, [input]);
 
   return (
-    <div className="bg-gradient-to-tr from-indigo-100 via-sky-100 to-purple-100 min-h-screen font-sans">
+    <div className="bg-gray-50  min-h-screen font-sans">
       <Navbar />
-      <div className="max-w-7xl mx-auto px-6 py-12">
-        <div className="bg-white/60 backdrop-blur-md border border-white/30 rounded-3xl shadow-2xl p-10">
-          <div className="flex flex-col md:flex-row items-center justify-between gap-6 mb-8">
+      <div className="max-w-6xl mx-auto p-6">
+        {/* <div className="bg-white/60 backdrop-blur-md border border-white/30 rounded-3xl shadow-2xl p-10"> */}
+          <div className="flex flex-col md:flex-row items-center justify-between gap-4 mb-6">
             <Input
-              className="w-full md:w-1/2 px-4 py-3 rounded-xl border border-gray-300 shadow-inner focus:ring-2 focus:ring-indigo-400 transition-all"
+              className="w-full md:w-1/2 lg:w-1/3 px-4 py-2 rounded-md border border-gray-300 shadow-sm"
               placeholder="Search by title or role..."
               onChange={(e) => setInput(e.target.value)}
             />
             <Button
-              className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-semibold px-6 py-3 rounded-xl shadow-lg hover:scale-105 transition-transform"
+              className="bg-[#6A38C2] text-white font-semibold px-6 py-3 rounded-xl shadow-lg hover:scale-105 transition-transform"
               onClick={() => navigate("/admin/jobs/create")}
             >
               + New Job
@@ -41,7 +41,7 @@ const AdminJobs = () => {
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   )
 }
 
